b6f10a8b046484ae8404c96b07901d0401cd9aef
Cusnir Sergiu <sergiu.cusnir@cst.ro>
2020-08-01 16:32:14 +0300

    Updates

---
+++ precache-manifest.65b68f00011730e12e7bb5af03ae82dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "99231f7cf0756103af68",
+    "url": "/monster-rolodex/static/css/main.f30471d6.chunk.css"
+  },
+  {
+    "revision": "99231f7cf0756103af68",
+    "url": "/monster-rolodex/static/js/main.d76189d4.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/monster-rolodex/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "82033b460dfdfa9cf59022ef57dbffcc",
+    "url": "/monster-rolodex/index.html"
+  }
+];
